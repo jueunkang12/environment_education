326e6edc9466cd1e3dbda4ff469355c698ebfdb2
Jueun Kang <jueun.kang@mail.utoronto.ca>
2023-03-30 13:44:31 -0400

    Reading in data for cleaning

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,20 +1,22 @@
 #### Preamble ####
 # Purpose: Cleans.... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Author: Jueun Kang
+# Data: 29 March 2023
+# Contact: jueun.kang@mail.utoronto.ca
+# Pre-requisites: N/A
+# Any other information needed? N/A
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(janitor)
+library(tidyr)
+library(haven)
+library(dplyr)
 
-#### Clean data ####
-# [...UPDATE THIS...]
-raw_data <- read_csv("inputs/data/raw_data.csv")
+#### Reads in data to be cleaned ####
+raw_data <- read_dta("~/Documents/environment_education/inputs/data/GSS2021.dta")
+
+
 
 
 
