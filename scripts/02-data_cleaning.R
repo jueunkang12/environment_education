2ef5af8133826491b9f1435f462a609f74666768
Jueun Kang <jueun.kang@mail.utoronto.ca>
2023-03-30 14:46:24 -0400

    Cleaning data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -32,9 +32,25 @@ clean_data <- clean_names(raw_data) |>
   )
   
 #### Clean data to show respondents' degree and count ####
-clean_data
+degrees_clean <- clean_data |>
+  select(
+    id,
+    degree
+  )
 
+#### Clean data to show respondents' willingness to pay towards environment ####
+willingness_clean <- clean_data |>
+  select(
+    id,
+    grnprice
+  )
 
+#### Clean data to show respondents who donated towards environmental group ####
+donation_clean <- clean_data |>
+  select(
+    id,
+    grnmoney
+  )
 
 #### Save data ####
 # [...UPDATE THIS...]
